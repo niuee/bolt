c8f4bca757c9849df0c1dd09d78cb737d3e67326
niuee <vntchang@gmail.com>
2023-12-11 15:58:08 +0800

    Change the output of the definition file for types to the corret directory

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default [{
 },
 {
     input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "es" }],
+    output: [{ file: "build/types.d.ts", format: "es" }],
     plugins: [dts.default()],
   } 
 ];
